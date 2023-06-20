590e4a0a69d3b8aa752019ebb0caabcd5c686418
C3h1c <fivemsloofficial@gmail.com>
2023-06-20 19:48:41 +0200

    support for GlobalState!
    idk, if gonna this works, but I'm testing..

--- server/main.lua
+++ server/main.lua
@@ -1,9 +1,12 @@
+-- Global state table
+local globalState = {}
+
 if ESX.GetConfig().OxInventory then
 	AddEventHandler('onServerResourceStart', function(resourceName)
 		if resourceName == 'ox_inventory' or resourceName == GetCurrentResourceName() then
 			local stashes = MySQL.query.await('SELECT * FROM addon_inventory')
 
-			for i=1, #stashes do
+			for i = 1, #stashes do
 				local stash = stashes[i]
 				local jobStash = stash.name:find('society') and string.sub(stash.name, 9)
 				exports.ox_inventory:RegisterStash(stash.name, stash.label, 100, 200000, stash.shared == 0 and true or false, jobStash)
@@ -11,99 +14,96 @@ if ESX.GetConfig().OxInventory then
 		end
 	end)
 
-	return
-end
+	globalState.Items = {} -- Global state variable
+	globalState.InventoriesIndex, globalState.Inventories, globalState.SharedInventories = {}, {}, {}
 
-Items = {}
-local InventoriesIndex, Inventories, SharedInventories = {}, {}, {}
+	MySQL.ready(function()
+		local items = MySQL.query.await('SELECT * FROM items')
 
-MySQL.ready(function()
-	local items = MySQL.query.await('SELECT * FROM items')
+		for i = 1, #items, 1 do
+			globalState.Items[items[i].name] = items[i].label
+		end
 
-	for i=1, #items, 1 do
-		Items[items[i].name] = items[i].label
-	end
+		local result = MySQL.query.await('SELECT * FROM addon_inventory')
 
-	local result = MySQL.query.await('SELECT * FROM addon_inventory')
+		for i = 1, #result, 1 do
+			local name = result[i].name
+			local label = result[i].label
+			local shared = result[i].shared
 
-	for i=1, #result, 1 do
-		local name   = result[i].name
-		local label  = result[i].label
-		local shared = result[i].shared
+			local result2 = MySQL.query.await('SELECT * FROM addon_inventory_items WHERE inventory_name = @inventory_name', {
+				['@inventory_name'] = name
+			})
 
-		local result2 = MySQL.query.await('SELECT * FROM addon_inventory_items WHERE inventory_name = @inventory_name', {
-			['@inventory_name'] = name
-		})
+			if shared == 0 then
+				table.insert(globalState.InventoriesIndex, name)
 
-		if shared == 0 then
+				globalState.Inventories[name] = {}
+				local items = {}
 
-			table.insert(InventoriesIndex, name)
+				for j = 1, #result2, 1 do
+					local itemName = result2[j].name
+					local itemCount = result2[j].count
+					local itemOwner = result2[j].owner
 
-			Inventories[name] = {}
-			local items       = {}
+					if items[itemOwner] == nil then
+						items[itemOwner] = {}
+					end
 
-			for j=1, #result2, 1 do
-				local itemName  = result2[j].name
-				local itemCount = result2[j].count
-				local itemOwner = result2[j].owner
-
-				if items[itemOwner] == nil then
-					items[itemOwner] = {}
+					table.insert(items[itemOwner], {
+						name = itemName,
+						count = itemCount,
+						label = globalState.Items[itemName]
+					})
 				end
 
-				table.insert(items[itemOwner], {
-					name  = itemName,
-					count = itemCount,
-					label = Items[itemName]
-				})
+				for k, v in pairs(items) do
+					local addonInventory = CreateAddonInventory(name, k, v)
+					table.insert(globalState.Inventories[name], addonInventory)
+				end
+			else
+				local items = {}
+
+				for j = 1, #result2, 1 do
+					table.insert(items, {
+						name = result2[j].name,
+						count = result2[j].count,
+						label = globalState.Items[result2[j].name]
+					})
+				end
+
+				local addonInventory = CreateAddonInventory(name, nil, items)
+				globalState.SharedInventories[name] = addonInventory
 			end
-
-			for k,v in pairs(items) do
-				local addonInventory = CreateAddonInventory(name, k, v)
-				table.insert(Inventories[name], addonInventory)
-			end
-
-		else
-			local items = {}
-
-			for j=1, #result2, 1 do
-				table.insert(items, {
-					name  = result2[j].name,
-					count = result2[j].count,
-					label = Items[result2[j].name]
-				})
-			end
-
-			local addonInventory    = CreateAddonInventory(name, nil, items)
-			SharedInventories[name] = addonInventory
 		end
-	end
-end)
+	end)
+end
 
 function GetInventory(name, owner)
-	for i=1, #Inventories[name], 1 do
-		if Inventories[name][i].owner == owner then
-			return Inventories[name][i]
+	for i = 1, #globalState.Inventories[name], 1 do
+		if globalState.Inventories[name][i].owner == owner then
+			return globalState.Inventories[name][i]
 		end
 	end
 end
 
 function GetSharedInventory(name)
-	return SharedInventories[name]
+	return globalState.SharedInventories[name]
 end
 
 function AddSharedInventory(society)
-    if type(society) ~= 'table' or not society?.name or not society?.label then return end
-    -- society (array) containing name (string) and label (string)
+	if type(society) ~= 'table' or not society?.name or not society?.label then
+		return
+	end
 
-    -- addon inventory:
-    MySQL.Async.execute('INSERT INTO addon_inventory (name, label, shared) VALUES (@name, @label, @shared)', {
-        ['name'] = society.name,
-        ['label'] = society.label,
-        ['shared'] = 1
-    })
+	-- Addon inventory
+	MySQL.Async.execute('INSERT INTO addon_inventory (name, label, shared) VALUES (@name, @label, @shared)', {
+		['name'] = society.name,
+		['label'] = society.label,
+		['shared'] = 1
+	})
 
-    SharedInventories[society.name] = CreateAddonInventory(society.name, nil, {})
+	globalState.SharedInventories[society.name] = CreateAddonInventory(society.name, nil, {})
 end
 
 AddEventHandler('esx_addoninventory:getInventory', function(name, owner, cb)
@@ -117,13 +117,13 @@ end)
 AddEventHandler('esx:playerLoaded', function(playerId, xPlayer)
 	local addonInventories = {}
 
-	for i=1, #InventoriesIndex, 1 do
-		local name      = InventoriesIndex[i]
+	for i = 1, #globalState.InventoriesIndex, 1 do
+		local name = globalState.InventoriesIndex[i]
 		local inventory = GetInventory(name, xPlayer.identifier)
 
 		if inventory == nil then
 			inventory = CreateAddonInventory(name, xPlayer.identifier, {})
-			table.insert(Inventories[name], inventory)
+			table.insert(globalState.Inventories[name], inventory)
 		end
 
 		table.insert(addonInventories, inventory)
